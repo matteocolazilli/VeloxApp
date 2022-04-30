a1e890e3fb6f6378eb6a252536fc1feb6fbaf782
matteocolazilli <matteocolazilli@users.noreply.github.com>
2022-04-30 23:10:55 +0200

    Aggiunto il controllo dell'abilitazione del GPS

--- app/src/main/java/it/univaq/veloxapp/fragments/MapFragment.java
+++ app/src/main/java/it/univaq/veloxapp/fragments/MapFragment.java
@@ -1,7 +1,11 @@
 package it.univaq.veloxapp.fragments;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -36,6 +41,7 @@ import it.univaq.veloxapp.utility.LocationHelper;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
+    private AlertDialog alertDialog;
     private GoogleMap map;
     private Marker myMarker;
     private List<Marker> autoveloxMarker = new ArrayList<>();
@@ -43,9 +49,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     private boolean firstUpdate = true;
     private Location myLocation = new Location("myposition");
 
-
-
-    private ActivityResultLauncher<String> launcher = registerForActivityResult(
+    private ActivityResultLauncher<String> locationPermissionLauncher = registerForActivityResult(
             new ActivityResultContracts.RequestPermission(),
             new ActivityResultCallback<Boolean>() {
                 @Override
@@ -53,10 +57,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
                     if (result){
                         locationHelper.start(MapFragment.this);
                     } else {
-                        Toast.makeText(requireContext(),R.string.disabled_position_toast, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(requireContext(),R.string.position_permissions_not_granted, Toast.LENGTH_SHORT).show();
+                        requireActivity().onBackPressed();
                     }
             }});
-
 
     @Nullable
     @Override
@@ -68,10 +72,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        locationHelper = new LocationHelper(requireContext(),launcher);
+        buildAlertMessageNoGps();
+        locationHelper = new LocationHelper(requireContext(), locationPermissionLauncher);
 
         SupportMapFragment fragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragmentMap);
         if (fragment != null) fragment.getMapAsync(this);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(alertDialog.isShowing()) alertDialog.dismiss();
+        checkGPSEnabled();
+        locationHelper.start(this);
     }
 
     @Override
@@ -84,24 +98,19 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
         map = googleMap;
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(@NonNull Marker marker) {
+        map.setOnMarkerClickListener(marker -> {
 
-                Autovelox autovelox = (Autovelox) marker.getTag();
-                if (autovelox != null) {
+            Autovelox autovelox = (Autovelox) marker.getTag();
+            if (autovelox != null) {
 
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("autovelox", autovelox);
-                    Navigation.findNavController(MapFragment.this.requireView()).navigate(R.id.action_navMap_to_detailActivity,bundle);
-                    return true;
-                }
-                return false;
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("autovelox", autovelox);
+                Navigation.findNavController(MapFragment.this.requireView()).navigate(R.id.action_navMap_to_detailActivity,bundle);
+                return true;
             }
+            return false;
         });
-
         locationHelper.start(this);
-
     }
 
     @Override
@@ -161,7 +170,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
                 requireActivity().runOnUiThread(()-> {
                             if (firstUpdate) {
                                 map.moveCamera(CameraUpdateFactory.newLatLngZoom(myMarker.getPosition(), 3));
-                                map.animateCamera(CameraUpdateFactory.zoomTo(11), 2000, null);
+                                map.animateCamera(CameraUpdateFactory.zoomTo(11), 3000, null);
                                 firstUpdate = false;
                             } else {
                                 map.animateCamera(CameraUpdateFactory.newLatLng(myMarker.getPosition()));
@@ -187,5 +196,30 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
             myMarker.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
         }
     }
+
+    public void checkGPSEnabled() {
+        if (!locationHelper.getLocationManager().isProviderEnabled(LocationManager.GPS_PROVIDER))
+            alertDialog.show();
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setMessage(R.string.no_gps_alert_message)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        requireContext().startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.dismiss();
+                        requireActivity().onBackPressed();
+                        Toast.makeText(requireContext(), R.string.disabled_position_toast, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        alertDialog = builder.create();
+    }
+
 }
 
