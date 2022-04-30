c870338b0a6c31edc73abcd9cc12f381d7f91d38
matteocolazilli <matteocolazilli@users.noreply.github.com>
2022-05-01 00:19:24 +0200

    Aggiunto la mappa nella DetailActivity

--- app/src/main/java/it/univaq/veloxapp/fragments/MapFragment.java
+++ app/src/main/java/it/univaq/veloxapp/fragments/MapFragment.java
@@ -27,7 +27,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
